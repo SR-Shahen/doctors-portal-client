5e14f0ca81803ddb339da38c030bb23610a2970b
SR-Shahen <s.r.shahenda@gmail.com>
2022-05-13 17:38:51 +0600

    added simple navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home';
+import Header from './Pages/Shared/Header'
+
 
 function App() {
   return (
     <div >
+      <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
       </Routes>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h4>This is home page</h4>
+            <h4 >This is home page</h4>
+
         </div>
     );
 };
